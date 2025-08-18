8035acc8b86b21593ae61b33e54760f538144a17
ydc-p-dev <ydc.dev4@gmail.com>
2025-08-18 11:29:25 +0200

    Enhance coupon validation logic to support API checks
    - Integrated Axios for making API requests to validate coupons.
    - Added logic to handle both website and API validation based on site configuration.
    - Improved error handling and logging for better debugging.
    - Updated package.json and package-lock.json to include Axios as a dependency.

--- coupon_validation/index.js
+++ coupon_validation/index.js
@@ -1,5 +1,6 @@
 const { firefox } = require('playwright');
 const fs = require('fs');
+const axios = require('axios');
 const actions = require('./actions.json');
 require('dotenv').config();
 let logs = [];
@@ -37,7 +38,7 @@ let page;
             siteConfig.productUrl = used_on_product_url;
         }
 
-        const proxy = process.env.PROXY_SERVER
+        let proxy = process.env.PROXY_SERVER
             ? {
                 server: process.env.PROXY_SERVER,
                 username: process.env.PROXY_USERNAME || undefined,
@@ -45,116 +46,120 @@ let page;
             }
             : undefined;
 
+        if (siteConfig.type == 'api'){
+            let couponIsValid = false;
+            try {
+                siteConfig.params = JSON.parse(JSON.stringify(siteConfig.params).replaceAll('{{COUPON}}', coupon));
+                log(`[🌐] Go to Api ${siteConfig.apiUrl}`);
+                let apiResult = await getApiData(siteConfig.apiUrl, siteConfig.params, proxy);
+                let response = JSON.stringify(apiResult);
+                if (response.indexOf(siteConfig.codeValidation.validText) > -1) {
+                    couponIsValid = true;
+                    log('[🎉🎉🎉] Coupon is valid!');
+                } else {
+                    log('[❌❌❌] Coupon is not valid.');
+                }
+            }catch (e) {
+                console.log('e',e);
+                log('[❌❌❌] There is a problem with the script.');
+            }
+            fs.writeFileSync('./output/result.json', JSON.stringify({logs, couponIsValid: couponIsValid}, null, 2));
+        }else {
+            log('[⏳] Starting headless-browser...');
+            const userDataDir = './pw-user';
 
-        log('[⏳] Starting headless-browser...');
-        const userDataDir = './pw-user';
+            const browserCtx = await firefox.launchPersistentContext(userDataDir, {
+                headless: true,
+                ...(proxy && {proxy}),
+                locale: 'en-US',
+                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.188 Safari/537.36',
+            });
 
-        const browserCtx = await firefox.launchPersistentContext(userDataDir, {
-            headless: true,
-            ...(proxy && { proxy }),
-            locale: 'en-US',
-            userAgent: 'Mozilla/5.0 ...'
-        });
+            page = browserCtx.pages()[0];
 
-        page = browserCtx.pages()[0]; // перша вкладка, що вже відкрита
+            await page.addInitScript(() => {
+                Object.defineProperty(navigator, 'webdriver', {get: () => false});
+                window.navigator.chrome = {runtime: {}};
+                Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});
+                Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
+            });
 
-        // const browser = await firefox.launch({
-        //     headless: false,
-        //     ...(proxy && {proxy})
-        // });
-        //
-        // const context = await browser.newContext({
-        //     userAgent:
-        //         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.188 Safari/537.36',
-        //     locale: 'en-US',
-        // });
-        //
-        // page = await context.newPage();
+            let couponIsValid = false;
 
+            try {
+                log(`[🌐] Go to Website ${siteConfig.productUrl}`);
+                await page.goto(siteConfig.productUrl, {waitUntil: 'domcontentloaded', timeout: 60000});
+                await page.waitForLoadState('networkidle', {timeout: 3000}).catch(() => {
+                });
+                await page.waitForTimeout(siteConfig.waitTime);
 
-
-        // 🛡️ Anti-bot evasion
-        await page.addInitScript(() => {
-            Object.defineProperty(navigator, 'webdriver', {get: () => false});
-            window.navigator.chrome = {runtime: {}};
-            Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});
-            Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
-        });
-
-        let couponIsValid = false;
-
-        try {
-            log(`[🌐] Go to Website ${siteConfig.productUrl}`);
-            await page.goto(siteConfig.productUrl, {waitUntil: 'domcontentloaded', timeout: 60000});
-            await page.waitForLoadState('networkidle', { timeout: 3000 }).catch(() => {});
-            await page.waitForTimeout(siteConfig.waitTime);
-
-            if (siteConfig.actions.length) {
-                for (let action of siteConfig.actions) {
-                    log(`[👉] Action: ${action.name}`);
-                    log(action.event);
-                    if (action.selectors.length > 0) {
-                        for (let selector of action.selectors) {
-                            try {
-                                let issetSelector = await retryWaitForSelector(page, selector, {
-                                    timeout: action.waitAfter,
-                                    state: 'attached'
-                                }, 5, 1000, action.required);
-                                if (issetSelector) {
-                                    if (action.type === 'fill') {
-                                        await page.fill(selector, coupon, {timeout: action.waitAfter});
-                                        await page.dispatchEvent(selector, 'input');
-                                        await page.dispatchEvent(selector, 'change');
-                                    } else if (action.type === 'click') {
-                                        const el = await page.$(selector);
-                                        if (el) {
-                                            await el.evaluate(el => el.click());
+                if (siteConfig.actions.length) {
+                    for (let action of siteConfig.actions) {
+                        log(`[👉] Action: ${action.name}`);
+                        log(action.event);
+                        if (action.selectors.length > 0) {
+                            for (let selector of action.selectors) {
+                                try {
+                                    let issetSelector = await retryWaitForSelector(page, selector, {
+                                        timeout: action.waitAfter,
+                                        state: 'attached'
+                                    }, 5, 1000, action.required);
+                                    if (issetSelector) {
+                                        if (action.type === 'fill') {
+                                            await page.fill(selector, coupon, {timeout: action.waitAfter});
+                                            await page.dispatchEvent(selector, 'input');
+                                            await page.dispatchEvent(selector, 'change');
+                                        } else if (action.type === 'click') {
+                                            const el = await page.$(selector);
+                                            if (el) {
+                                                await el.evaluate(el => el.click());
+                                            }
+                                        } else {
+                                            await page[action.type](selector, {timeout: action.waitAfter, force: true});
                                         }
-                                    } else {
-                                        await page[action.type](selector, {timeout: action.waitAfter, force: true});
+                                        if (action.waitAfter) {
+                                            log(`⏳ Waiting ${action.waitAfter}ms after action`);
+                                            await new Promise(resolve => setTimeout(resolve, action.waitAfter));
+                                        }
                                     }
-                                    if (action.waitAfter) {
-                                        log(`⏳ Waiting ${action.waitAfter}ms after action`);
-                                        await new Promise(resolve => setTimeout(resolve, action.waitAfter));
-                                    }
+                                } catch (e) {
+                                    break;
+                                    error(`[⚠️] Failed action "${action.name}" on selector "${selector}": ${e.message}`);
                                 }
-                            } catch (e) {
-                                break;
-                                error(`[⚠️] Failed action "${action.name}" on selector "${selector}": ${e.message}`);
                             }
                         }
                     }
                 }
-            }
 
-            await page.waitForTimeout(siteConfig.waitTime);
+                await page.waitForTimeout(siteConfig.waitTime);
 
-            const element = await page.$(siteConfig.promoCode.elementAlert);
-            if (element) {
-                const text = await element.innerText();
-                if (text.includes(siteConfig.promoCode.validText)) {
-                    log('[🎉🎉🎉] Coupon is valid!');
-                    couponIsValid = true;
+                const element = await page.$(siteConfig.codeValidation.element);
+                if (element) {
+                    const text = await element.innerText();
+                    if (text.includes(siteConfig.codeValidation.validText)) {
+                        log('[🎉🎉🎉] Coupon is valid!');
+                        couponIsValid = true;
+                    } else {
+                        log('[❌❌❌] Coupon is not valid.');
+                    }
                 } else {
                     log('[❌❌❌] Coupon is not valid.');
                 }
-            } else {
-                log('[❌❌❌] Coupon is not valid.');
-            }
 
-        } catch (e) {
-            error(`❌ Unexpected error: ${e.message}`);
+            } catch (e) {
+                error(`❌ Unexpected error: ${e.message}`);
+            }
+            await clearSiteStorage(page);
+            const outputDir = './output';
+            if (!fs.existsSync(outputDir)) {
+                fs.mkdirSync(outputDir, {recursive: true});
+            }
+            const html = await page.content();
+            await page.screenshot({path: `${outputDir}/screenshot.png`, fullPage: true});
+            fs.writeFileSync(`${outputDir}/html_snapshot.html`, html);
+            fs.writeFileSync(`${outputDir}/result.json`, JSON.stringify({logs, couponIsValid}, null, 2));
+            await browserCtx.close();
         }
-        await clearSiteStorage(page);
-        const outputDir = './output';
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, {recursive: true});
-        }
-        const html = await page.content();
-        await page.screenshot({path: `${outputDir}/screenshot.png`, fullPage: true});
-        fs.writeFileSync(`${outputDir}/html_snapshot.html`, html);
-        fs.writeFileSync(`${outputDir}/result.json`, JSON.stringify({logs, couponIsValid}, null, 2));
-        await browserCtx.close();
     })();
 
 async function clearSiteStorage(page) {
@@ -221,6 +226,7 @@ async function retryWaitForSelector(page, selector, options = {}, maxAttempts = 
         try {
             return await page.waitForSelector(selector, options);
         } catch (e) {
+            console.log('e',e);
             log(`🔁 Attempt ${attempt} failed for selector "${selector}"`);
             if (attempt === maxAttempts || !required) {
                 log(`Selector "${selector}" not found after ${maxAttempts} attempts.`);
@@ -228,5 +234,30 @@ async function retryWaitForSelector(page, selector, options = {}, maxAttempts = 
             }
             await new Promise(res => setTimeout(res, delayBetween));
         }
+    }
+}
+
+async function getApiData(url, params = {}, proxy = undefined){
+    let axiosConfig = {};
+    if (proxy) {
+        axiosConfig.proxy = {
+            protocol: process.env.PROXY_PROTOCOL ? process.env.PROXY_PROTOCOL : 'http', // http або https
+            host: process.env.PROXY_SERVER ?  process.env.PROXY_SERVER.split(':')[0] : null,
+            port: process.env.PROXY_SERVER ?  process.env.PROXY_SERVER.split(':')[1] : null,
+            auth:
+                proxy.username && proxy.password
+                    ? { username: proxy.username, password: proxy.password }
+                    : undefined
+        };
+    }
+    try {
+        const res = await axios.post(url, params,{
+            ...axiosConfig,
+        });
+
+        return res.data;
+    } catch (err) {
+        error(err.message);
+        return [];
     }
 }
